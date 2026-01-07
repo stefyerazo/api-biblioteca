d39d1d942f0e44117f0d1d90d76d4506d746e1d8
stefyerazo <scerazo1@espe.edu.ec>
2026-01-07 12:48:27 -0500

    Cambios biblioteca

--- models/database.js
+++ models/database.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD,{
     host:process.env.DB_HOST,
-    dialect:'postgres',
+    dialect:'mysql',
     port: process.env.DB_PORT
 });
 
